4198ead9d92e7ee2ce26409bc703d35e461856fb
mra <martin.raenker@imtb.de>
2016-05-20 19:23:06 +0200

    Alle Dateien in src/ gelöscht. requirements.txt auf Django 1.8.13 eingestellt.

--- src/index/urls.py
+++
@@ -1,8 +0,0 @@
-from django.conf.urls import url
-from . import views
-
-urlpatterns = [
-    url(r'^schueler_list$', views.schueler_list, name='schueler_list'),
-    #url(r'^base', views.base, name='base'),
-    url(r'', views.index),
-]
