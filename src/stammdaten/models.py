538e5dee6303974068e5e569f5335ff9d89063d9
mra <martin.raenker@imtb.de>
2016-06-09 14:10:45 +0200

    stammdaten: model angepasst für ersten versuch abrechnungslogik.

--- src/stammdaten/models.py
+++ src/stammdaten/models.py
@@ -1,125 +1,92 @@
 from __future__ import unicode_literals
 from django.db import models
 from django.utils.timezone import datetime
-from django.utils.formats import localize
-from django.utils.translation import ugettext
 
 
+# TODO: CONSTRAINTS mit clean() (siehe ModelValidation in DjangoDocs)
 
 class Gruppe(models.Model):
-    gruppenname = models.CharField(max_length=400, default="")
+    name = models.CharField(max_length=400, default="")
     bemerkungen = models.CharField(max_length=200)
-    
-    #erstellungsdatum = models.DateTimeField(auto_now_add = True, default="", editable=False)
-    erstellungsdatum = models.DateTimeField(default=datetime.now, editable=False)
+    erstellungsdatum = models.DateTimeField(auto_now_add=True, editable=False)
     
     class Meta:
       verbose_name_plural="Gruppen"
       verbose_name="Gruppe"  
     
     def __unicode__(self):
-       return self.gruppenname
-
-
+       return self.name
 
 
 class Stufe(models.Model):
-    stufenname = models.CharField(max_length=400, default="")
+    name = models.CharField(max_length=400, default="")
     bemerkungen = models.CharField(max_length=200)
-    
-    #erstellungsdatum = models.DateTimeField(auto_now_add = True, default="", editable=False)
-    erstellungsdatum = models.DateTimeField(default=datetime.now, editable=False)
+    erstellungsdatum = models.DateTimeField(auto_now_add=True, editable=False)
         
     class Meta:
       verbose_name_plural="Stufen"
       verbose_name="Stufe"
       
     def __unicode__(self):
-        return self.stufenname
-
-
+        return self.name
 
 
 class Einrichtung(models.Model):
-    einrichtungsname = models.CharField(max_length=200, default="")
-    einrichtungspflegesatz_schultag = models.DecimalField(max_digits=4, decimal_places=2)
-    einrichtungspflegesatz_schultag_startdatum = models.DateField()
-    einrichtungspflegesatz_schultag_enddatum = models.DateField()
-    einrichtungspflegesatz_ferientag = models.DecimalField(max_digits=4, decimal_places=2)
-    einrichtungspflegesatz_ferientag_startdatum = models.DateField()
-    einrichtungspflegesatz_ferientag_enddatum = models.DateField()
-    
-    #erstellungsdatum = models.DateTimeField(auto_now_add = True, default="", editable=False)
-    erstellungsdatum = models.DateTimeField(default=datetime.now, editable=False)
+    name = models.CharField(max_length=200)
+    pflegesatz = models.DecimalField(max_digits=4, decimal_places=2)
+    erstellungsdatum = models.DateTimeField(auto_now_add=True, editable=False)
         
     class Meta:
       verbose_name_plural="Einrichtungen"
       verbose_name="Einrichtung"  
     
     def __unicode__(self):
-        return self.einrichtungsname
+        return self.name
 
 
-
-
-class Sozialamt(models.Model):
-    sozialamtname = models.CharField(max_length=200, default="")
-    sozialamt_anschrift = models.CharField(max_length=400)
-    sozialamt_konto_iban = models.CharField(max_length=22)
-    sozialamt_konto_institut = models.CharField(max_length=400)
+class Ferien(models.Model):
+    name = models.CharField(max_length=200)
+    startdatum = models.DateField()
+    enddatum = models.DateField()
+    einrichtungen = models.ManyToManyField(Einrichtung, verbose_name='Einrichtungen', related_name='ferien')
+    erstellungsdatum = models.DateTimeField(auto_now_add=True, editable=False)
     
-    #erstellungsdatum = models.DateTimeField(auto_now_add = True, default="", editable=False)
-    erstellungsdatum = models.DateTimeField(default=datetime.now, editable=False) 
+    class Meta:
+      verbose_name_plural="Ferien"
+      verbose_name="Ferien"  
+    
+    def __unicode__(self):
+        return self.name    
+
+    
+class Sozialamt(models.Model):
+    name = models.CharField(max_length=200, default="")
+    anschrift = models.CharField(max_length=400)
+    konto_iban = models.CharField(max_length=22)
+    konto_institut = models.CharField(max_length=400)
+    erstellungsdatum = models.DateTimeField(auto_now_add=True, editable=False)
     
     class Meta:
       verbose_name_plural="Sozialaemter"
       verbose_name="Sozialamt"
     
     def __unicode__(self):
-        return self.sozialamtname
+        return self.name
     
-    
- 
-    
+
 class Schliesstag(models.Model):
     datum = models.DateField()
     art = models.CharField(max_length=30, blank=True)
     name = models.CharField(max_length=50, blank=True)
-    
-    #erstellungsdatum = models.DateTimeField(auto_now_add = True, default="", editable=False)
-    erstellungsdatum = models.DateTimeField(default=datetime.now, editable=False)
+    einrichtungen = models.ManyToManyField(Einrichtung, verbose_name='Einrichtungen', related_name='schliesstage')
     
     class Meta:
       verbose_name_plural="Schliesstage"
-      verbose_name="Schliesstag"
+      verbose_name="Schliesstag"  
     
     def __unicode__(self):
-        return ugettext("%(datum)s") % {
-            'datum': localize(self.datum),
-        }
-    
-    
-    
-    
-class Ferien(models.Model):
-    ferienname = models.CharField(max_length=30, blank=True)
-    startdatum = models.DateField()
-    enddatum = models.DateField()
-    
-    #erstellungsdatum = models.DateTimeField(auto_now_add = True, default="", editable=False)
-    erstellungsdatum = models.DateTimeField(default=datetime.now, editable=False)
+        return '{s.name}: {s.datum}) '.format(s=self)
         
-    class Meta:
-      verbose_name_plural="Ferien"
-      verbose_name="Ferien"
-    
-    def __unicode__(self):
-        return (self.ferienname) + ' ' + ugettext("vom %(startdatum)s bis %(enddatum)s") % {
-            'startdatum': localize(self.startdatum),
-            'enddatum': localize(self.enddatum),
-            }
-
-
 
 class Schueler(models.Model):
     
@@ -127,43 +94,38 @@ class Schueler(models.Model):
     vorname = models.CharField(max_length=200)
     geburtsdatum = models.DateField()
     bemerkungen = models.TextField(max_length=10000)
-    pers_pflegesatz = models.DecimalField(max_digits=4, decimal_places=2, blank=True)
-    buchungsnummer = models.CharField(max_length=30, blank=True, null=True, default="")
-    
-    stufe = models.ForeignKey(Stufe, on_delete=models.CASCADE, default="")
-    
-    #erstellungsdatum = models.DateTimeField(auto_now_add = True, default="", editable=False)
-    erstellungsdatum = models.DateTimeField(default=datetime.now, editable=False)
+    pers_pflegesatz = models.DecimalField(max_digits=4, decimal_places=2, default=0)
+    buchungsnummer = models.CharField(max_length=30, blank=True)
+    stufe = models.ForeignKey(Stufe)
+    gruppe = models.ForeignKey(Gruppe)
+    sozialamt = models.ForeignKey(Sozialamt)
+    einrichtungen = models.ManyToManyField(Einrichtung, through='SchuelerInEinrichtung')
+    erstellungsdatum = models.DateTimeField(auto_now_add=True, editable=False)
         
     class Meta:
       verbose_name_plural="Schueler"
       verbose_name="Schueler"
 
     def __unicode__(self):
-        return (self.nachname) + ', ' + (self.vorname)
-
-
+        return '{s.nachname}, {s.vorname}'.format(s=self)
 
 
 #TODO: class PflegesatzDesSchuelers mittels model manager und class realisieren
 
-
-
     
 class SchuelerInEinrichtung(models.Model):
 
-    schueler = models.ForeignKey(Schueler, on_delete=models.CASCADE, default="", related_name='+')
-    einrichtung = models.ForeignKey(Einrichtung, on_delete=models.CASCADE, default="", related_name='+')
-
-    erstellungsdatum = models.DateTimeField(default=datetime.now, editable=False)
+    schueler = models.ForeignKey(Schueler)
+    einrichtung = models.ForeignKey(Einrichtung)
+    eintritt = models.DateField()
+    austritt = models.DateField()
+    sozialamtbescheid_von = models.DateField()
+    sozialamtbescheid_bis = models.DateField()
     
     class Meta:
-        verbose_name="Schueler in der Einrichtung"
-        verbose_name_plural="Schueler in der Einrichtung"
+        unique_together = ('schueler', 'einrichtung', 'eintritt', 'austritt')
+        verbose_name_plural = "Schueler in den Einrichtungen"
+        verbose_name = "Schueler in der Einrichtung"
         
     def __unicode__(self):
-        return ugettext("%(schueler)s in %(einrichtung)s") % {
-            'schueler': localize(self.schueler),
-            'einrichtung': localize(self.einrichtung)
-            }
-    
+        return '{s.schueler} in {s.einrichtung} ({s.eintritt} - {s.austritt}) '.format(s=self)
