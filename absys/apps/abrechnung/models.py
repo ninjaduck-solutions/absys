7d84973fef1a75ba717a6a9535a14e550b8a3b15
Martin Raenker <martin.raenker@imtb.de>
2016-06-23 07:32:29 +0000

    models: Formatierung geaendert

--- absys/apps/abrechnung/models.py
+++ absys/apps/abrechnung/models.py
@@ -2,34 +2,34 @@ from django.db import models
 #from django.utils.timezone import now
 from django.utils.functional import cached_property
 #from datetime import datetime
- 
+
 # # TODO: CONSTRAINTS mit clean() (siehe ModelValidation in DjangoDocs)
 
- 
+
 class Gruppe(models.Model):
-    
+
     name = models.CharField(max_length=5, default="")
     bemerkungen = models.CharField(max_length=200)
     erstellungsdatum = models.DateTimeField(auto_now_add=True, editable=False)
-    
+
     class Meta:
       verbose_name_plural="Gruppen"
-      verbose_name="Gruppe"  
-    
+      verbose_name="Gruppe"
+
     def __str__(self):
        return self.name
 
 
 class Stufe(models.Model):
-    
+
     name = models.CharField(max_length=5, default="")
     bemerkungen = models.CharField(max_length=200)
     erstellungsdatum = models.DateTimeField(auto_now_add=True, editable=False)
-        
+
     class Meta:
       verbose_name_plural="Stufen"
       verbose_name="Stufe"
-      
+
     def __str__(self):
         return self.name
 
@@ -45,64 +45,64 @@ class Einrichtung(models.Model):
     pflegesatz_ferien_startdatum = models.DateField()
     pflegesatz_ferien_enddatum = models.DateField()
     erstellungsdatum = models.DateTimeField(auto_now_add=True, editable=False)
-        
+
     class Meta:
       verbose_name_plural="Einrichtungen"
-      verbose_name="Einrichtung"  
-    
+      verbose_name="Einrichtung"
+
     def __str__(self):
         return self.kuerzel
 
 
 class Ferien(models.Model):
-    
+
     name = models.CharField(max_length=10)
     startdatum = models.DateField()
     enddatum = models.DateField()
     einrichtungen = models.ManyToManyField(Einrichtung, verbose_name='Einrichtungen', related_name='ferien')
     erstellungsdatum = models.DateTimeField(auto_now_add=True, editable=False)
-    
+
     class Meta:
       verbose_name_plural="Ferien"
-      verbose_name="Ferien"  
-    
+      verbose_name="Ferien"
+
     def __str__(self):
         return self.name
 
-    
+
 class Sozialamt(models.Model):
-    
+
     name = models.CharField(max_length=10, default="")
     anschrift = models.CharField(max_length=20)
     konto_iban = models.CharField(max_length=22)
     konto_institut = models.CharField(max_length=10)
     erstellungsdatum = models.DateTimeField(auto_now_add=True, editable=False)
-    
+
     class Meta:
       verbose_name_plural="Sozialaemter"
       verbose_name="Sozialamt"
-    
+
     def __str__(self):
         return self.name
-    
+
 
 class Schliesstag(models.Model):
-    
+
     datum = models.DateField()
     art = models.CharField(max_length=10, blank=True)
     name = models.CharField(max_length=5, blank=True)
     einrichtungen = models.ManyToManyField(Einrichtung, verbose_name='Einrichtungen', related_name='schliesstage')
-    
+
     class Meta:
       verbose_name_plural="Schliesstage"
-      verbose_name="Schliesstag"      
-    
+      verbose_name="Schliesstag"
+
     def __str__(self):
         return '{s.name}: {s.datum}) '.format(s=self)
 
 
 class Schueler(models.Model):
-    
+
     nachname = models.CharField(max_length=10)
     vorname = models.CharField(max_length=10)
     geburtsdatum = models.DateField()
@@ -113,7 +113,7 @@ class Schueler(models.Model):
     sozialamt = models.ForeignKey(Sozialamt)
     einrichtungen = models.ManyToManyField(Einrichtung, through='SchuelerInEinrichtung')
     erstellungsdatum = models.DateTimeField(auto_now_add=True, editable=False)
-    
+
     class Meta:
       verbose_name_plural="Schueler"
       verbose_name="Schueler"
@@ -123,12 +123,12 @@ class Schueler(models.Model):
 
 
 class FehltageSchuelerErlaubt(models.Model):
-    
+
     schueler = models.ForeignKey(Schueler)
-    wert = models.PositiveIntegerField(default=45) 
+    wert = models.PositiveIntegerField(default=45)
     startdatum = models.DateField()
     enddatum = models.DateField()
-    
+
     class Meta:
         verbose_name_plural = "Erlaubte Fehltage von Schuelern"
         verbose_name = "Erlaubte Fehltage eines Schuelers"
@@ -151,13 +151,13 @@ class SchuelerInEinrichtung(models.Model):
     pers_pflegesatz_ferien = models.DecimalField(max_digits=4, decimal_places=2, default=0)
     pers_pflegesatz_ferien_startdatum = models.DateField(blank=True, null=True)
     pers_pflegesatz_ferien_enddatum = models.DateField(blank=True, null=True)
-    
+
     class Meta:
         unique_together = ('schueler', 'einrichtung', 'eintritt', 'austritt')
         verbose_name_plural = "Schueler in den Einrichtungen"
         verbose_name = "Schueler in der Einrichtung"
         ordering = ("schueler__nachname", "schueler__vorname", "-austritt")
-        
+
     def __str__(self):
         return '{s.schueler} in {s.einrichtung} ({s.eintritt} - {s.austritt}) '.format(s=self)
 
@@ -175,7 +175,7 @@ class Anwesenheit(models.Model):
     datum = models.DateField()
     anwesenheit = models.CharField(max_length=1)
     abgerechnet = models.BooleanField(default=False)
-    
+
     class Meta:
         verbose_name_plural = "Anwesenheiten der Schueler"
         verbose_name = "Anwesenheit eines Schueler in einer Einrichtung"
@@ -183,7 +183,7 @@ class Anwesenheit(models.Model):
 
     def __str__(self):
         return  '{s.schueler} am {s.datum} | {s.anwesenheit}'.format(s=self)
-    
+
     #def anzahlAnwesenheit
-    
+
     #def anzahlFehltage
