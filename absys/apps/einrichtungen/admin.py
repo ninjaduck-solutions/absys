31bf5910d3e6cb3b4a9829b7297a2274269b70c4
Markus Zapke-Gründemann <markus@transcode.de>
2016-06-24 17:29:29 +0200

    Admins erweitert

--- absys/apps/einrichtungen/admin.py
+++ absys/apps/einrichtungen/admin.py
@@ -3,8 +3,22 @@ from django.contrib import admin
 from . import models
 
 
+class SchuelerInEinrichtungAdmin(admin.TabularInline):
+    model = models.SchuelerInEinrichtung
+
+
+class EinrichtungHatPflegesatzAdmin(admin.TabularInline):
+    model = models.EinrichtungHatPflegesatz
+
+
 class EinrichtungAdmin(admin.ModelAdmin):
     list_display = ('name', 'kuerzel')
+    inlines = [
+        EinrichtungHatPflegesatzAdmin,
+        SchuelerInEinrichtungAdmin,
+    ]
 
 
 admin.site.register(models.Einrichtung, EinrichtungAdmin)
+admin.site.register(models.Ferien)
+admin.site.register(models.Schliesstag)
--- absys/apps/schueler/admin.py
+++ absys/apps/schueler/admin.py
@@ -10,4 +10,8 @@ class SchuelerAdmin(admin.ModelAdmin):
     search_fields = ['vorname', 'nachname']
 
 
+admin.site.register(models.FehltageSchuelerErlaubt)
+admin.site.register(models.Gruppe)
 admin.site.register(models.Schueler, SchuelerAdmin)
+admin.site.register(models.Sozialamt)
+admin.site.register(models.Stufe)
