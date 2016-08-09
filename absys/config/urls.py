80b20ac347b81092dc1d40696fb77ee57cfc4a50
Markus Zapke-Gründemann <markus@transcode.de>
2016-08-09 15:34:09 +0000

    Django admindocs aktiviert

--- absys/config/urls.py
+++ absys/config/urls.py
@@ -5,6 +5,7 @@ from django.conf.urls.static import static
 from django.contrib import admin
 
 urlpatterns = [
+    url(r'^admin/doc/', include('django.contrib.admindocs.urls')),
     url(r'^admin/', include(admin.site.urls)),
     url(r'^anwesenheitsliste/', include('absys.apps.anwesenheitsliste.urls')),
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
