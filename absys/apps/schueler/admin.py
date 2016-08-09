c029dc541001fc8c44e0a92aece64e50a3413833
Martin Raenker <vagrant@vagrant.vm>
2016-08-09 18:05:44 +0000

    schueler-model: import-export hinzugefuegt

--- absys/apps/schueler/admin.py
+++ absys/apps/schueler/admin.py
@@ -1,14 +1,24 @@
 from django.contrib import admin
 
+from import_export import resources
+from import_export.admin import ImportExportModelAdmin, ImportExportActionModelAdmin
+
 from . import models
 
 
-class SchuelerAdmin(admin.ModelAdmin):
+#class SchuelerResource(resources.ModelResource):
+#
+#    class Meta:
+#        model = models.Schueler
+
+class SchuelerAdmin(ImportExportModelAdmin):
     list_display = ('nachname', 'vorname', 'geburtsdatum', 'stufe', 'gruppe')
     list_display_links = ('nachname', 'vorname')
     list_filter = ('stufe', 'gruppe')
     search_fields = ['vorname', 'nachname']
 
+class SchuelerAdmin(ImportExportActionModelAdmin):
+    pass
 
 admin.site.register(models.Gruppe)
 admin.site.register(models.Schueler, SchuelerAdmin)
