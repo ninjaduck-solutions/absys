f41d706d5411f11c462468a2c45cda4ea8d1478f
Martin Raenker <vagrant@vagrant.vm>
2016-08-10 08:31:53 +0000

    django-import-export: foreign_fields eingeführt

--- absys/apps/anwesenheitsliste/admin.py
+++ absys/apps/anwesenheitsliste/admin.py
@@ -1,12 +1,29 @@
 from django.contrib import admin
 
+from import_export import resources
+from import_export.admin import ImportExportActionModelAdmin
+
 from . import models
 
 
-class AnwesenheitAdmin(admin.ModelAdmin):
+class AnwesenheitenResource(resources.ModelResource):
+
+    class Meta:
+    	model = models.Anwesenheit
+    	fields = (
+    		'schueler__vorname', 
+    		'schueler__nachname', 
+    		'schueler__buchungsnummer',
+    		'einrichtung__name',
+    		)
+
+
+class AnwesenheitAdmin(ImportExportActionModelAdmin):
+
     list_display = ('schueler', 'einrichtung', 'datum', 'abwesend')
     list_display_links = ('schueler', 'einrichtung')
     list_editable = ['abwesend']
 
+    resource_class = AnwesenheitenResource
 
 admin.site.register(models.Anwesenheit, AnwesenheitAdmin)
