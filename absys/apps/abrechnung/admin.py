2307069b81c4d41162307b1be1082d46346986c1
Martin Raenker <vagrant@vagrant.vm>
2016-08-11 09:55:36 +0000

    admin: Adminerweiterungen implementiert

--- absys/apps/abrechnung/admin.py
+++ absys/apps/abrechnung/admin.py
@@ -1,3 +1,29 @@
 from django.contrib import admin
 
-# Register your models here.
+from . import models
+
+from import_export.admin import ImportExportActionModelAdmin
+
+
+class RechnungAdmin(admin.TabularInline):
+    model = models.Rechnung
+
+
+#class RechnungsPositionAdmin(admin.ModelAdmin):
+#    model = models.RechnungsPosition
+
+
+class RechnungSozialamtAdmin(ImportExportActionModelAdmin):
+#    model = models.RechnungSozialamt
+
+    #raw_id_fields = ('nummer',)
+
+    list_display = ('sozialamt', 'startdatum', 'enddatum')
+    inlines = [
+        RechnungAdmin,
+    ]
+
+
+admin.site.register(models.Rechnung)
+#admin.site.register(models.RechnungsPosition, RechnungsPositionAdmin)
+admin.site.register(models.RechnungSozialamt, RechnungSozialamtAdmin)
--- absys/apps/anwesenheitsliste/admin.py
+++ absys/apps/anwesenheitsliste/admin.py
@@ -1,29 +1,43 @@
 from django.contrib import admin
 
-from import_export import resources
+from import_export import resources, fields
+
 from import_export.admin import ImportExportActionModelAdmin
+from import_export.widgets import ForeignKeyWidget
 
 from . import models
+
+from absys.apps.schueler.models import Schueler
+from absys.apps.einrichtungen.models import Einrichtung
+
 
 
 class AnwesenheitenResource(resources.ModelResource):
 
+    #einrichtung = fields.Field(column_name='einrichtung', attribute='einrichtung', widget=ForeignKeyWidget(Einrichtung, 'pk'))
+    #schueler = fields.Field(column_name='schueler', attribute='schueler', widget=ForeignKeyWidget(Schueler, 'pk'))
+
     class Meta:
-    	model = models.Anwesenheit
-    	fields = (
-    		'schueler__vorname', 
-    		'schueler__nachname', 
-    		'schueler__buchungsnummer',
-    		'einrichtung__name',
-    		)
 
+        model = models.Anwesenheit
 
-class AnwesenheitAdmin(ImportExportActionModelAdmin):
+        # fields = (
+        #     'id',
+        #     'datum',
+        #     'schueler',
+        #     'einrichtung',
+        #     'abwesend',
+        #     )
+
+class AnwesenheitAdmin(ImportExportActionModelAdmin, admin.ModelAdmin):
 
     list_display = ('schueler', 'einrichtung', 'datum', 'abwesend')
     list_display_links = ('schueler', 'einrichtung')
     list_editable = ['abwesend']
+    list_filter = ('datum', 'schueler')
+    search_fields = ['schueler__nachname', 'schueler__vorname', 'einrichtung__name', 'datum']
 
     resource_class = AnwesenheitenResource
+
 
 admin.site.register(models.Anwesenheit, AnwesenheitAdmin)
