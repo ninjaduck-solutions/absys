a5cf776be5f7081ca8ee1bee86442b372c595292
Markus Zapke-Gründemann <markus@transcode.de>
2016-08-19 16:51:43 +0000

    einrichtungen: SchuelerInEinrichtungAdmin hinzugefuegt

--- absys/apps/einrichtungen/admin.py
+++ absys/apps/einrichtungen/admin.py
@@ -4,8 +4,14 @@ from . import models
 
 from import_export.admin import ImportExportActionModelAdmin
 
+
+class SchuelerInEinrichtungAdmin(admin.ModelAdmin):
+    date_hierarchy = 'eintritt'
+    list_display = ('schueler', 'einrichtung', 'eintritt', 'austritt', 'fehltage_erlaubt')
+
+
 #TODO: dafuer sorgen, dass Zeilen richtig umgebrochen werden
-class SchuelerInEinrichtungAdmin(admin.TabularInline):
+class SchuelerInEinrichtungInline(admin.TabularInline):
     model = models.SchuelerInEinrichtung
     raw_id_fields = ('schueler',)
     extra = 0
@@ -20,10 +26,11 @@ class EinrichtungAdmin(ImportExportActionModelAdmin):
     list_display = ('name', 'kuerzel')
     inlines = [
         EinrichtungHatPflegesatzAdmin,
-        SchuelerInEinrichtungAdmin,
+        SchuelerInEinrichtungInline,
     ]
 
 
+admin.site.register(models.SchuelerInEinrichtung, SchuelerInEinrichtungAdmin)
 admin.site.register(models.Einrichtung, EinrichtungAdmin)
 admin.site.register(models.Ferien)
 admin.site.register(models.Schliesstag)
