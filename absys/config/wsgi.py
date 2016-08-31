a3ffac0bea5689b6e4f8704f8797fff12eda8455
Markus Zapke-Gründemann <markus@transcode.de>
2016-08-31 16:09:12 +0000

    Anpassungen fuer Apache und Envdir

--- absys/config/wsgi.py
+++ absys/config/wsgi.py
@@ -10,5 +10,9 @@ import os
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'absys.config.settings.dev')
 os.environ.setdefault('DJANGO_CONFIGURATION', 'Development')
 
+if 'APACHE_PID_FILE' in os.environ and os.path.exists(os.environ['APACHE_PID_FILE']):
+    import envdir
+    envdir.open('/var/envdir/absys')
+
 from configurations.wsgi import get_wsgi_application
 application = get_wsgi_application()
