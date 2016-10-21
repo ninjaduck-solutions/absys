d666150f4987c4b13576b39de604062f207b0968
Markus Zapke-Gründemann <markus@transcode.de>
2016-10-21 16:12:39 +0200

    extract.sh: Benutze Paket Versionen aus apt history

--- bin/extract.sh
+++ bin/extract.sh
@@ -6,13 +6,9 @@ set -o nounset
 set -o pipefail
 set -o verbose
 
-PKG_NAME_REGEXP="^Commandline: /usr/bin/apt-get -q -y -o DPkg::Options::=--force-confold -o DPkg::Options::=--force-confdef install (\K[a-z0-9]+[-+\.a-z0-9]+)$"
+PKG_NAME_REGEXP="^Commandline: /usr/bin/apt-get -q -y -o DPkg::Options::=--force-confold -o DPkg::Options::=--force-confdef install (\K(?:(?:[a-z0-9]+[-+\.a-z0-9]*=(?:[0-9]:)?[0-9]+[+\.:~a-z0-9]*(?:\-[+\.~0-9a-z]+)?)\s*)+)$"
 
-echo -n > /vagrant/setup/packages.txt
-PKGS=$(grep --color=never --only-matching --perl-regexp "${PKG_NAME_REGEXP}" /var/log/apt/history.log | sort)
-for PKG in ${PKGS} ;
-    do dpkg --status ${PKG} | grep 'Version' | echo "${PKG}=$(cut -c 10-)" >> /vagrant/setup/packages.txt ;
-done
+grep --color=never --only-matching --perl-regexp "${PKG_NAME_REGEXP}" /var/log/apt/history.log | tr " " "\n" > /vagrant/setup/packages.txt
 cp --verbose /etc/apache2/conf-available/wsgi.conf /vagrant/setup
 cp --verbose /etc/apache2/sites-available/django.conf /vagrant/setup
 cp --verbose /etc/postgresql/9.5/main/pg_hba.conf /vagrant/setup
