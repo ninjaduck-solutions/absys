14ec68259cd156bf12cb174835921d74036fef5e
Martin Raenker <martin.raenker@imtb.de>
2016-10-26 14:48:20 +0000

    abrechnung: SaxMBS-Template angepasst

--- absys/apps/abrechnung/templatetags/abrechnung_tags.py
+++ absys/apps/abrechnung/templatetags/abrechnung_tags.py
@@ -13,6 +13,7 @@ def decimal_ohne_punkt(decimal, kommastellen=2):
 @register.filter
 def integer_abgerundet(decimal, kommastellen=2):
     """
-    Gibt einen Decimal ohne Punkt als Integer zurück.
+    Gibt einen Decimal als Integer zurück. Die Kommastellen 
+    werden werden abgeschnitten.
     """
     return int(decimal)
