ff8dbd3e48e1e9bac678ebfd33a0693c8c808c75
Markus Zapke-Gründemann <markus@transcode.de>
2016-12-02 19:38:49 +0100

    abrechnung: Fehler beim Zugriff auf einrichtung behoben

--- absys/apps/abrechnung/responses.py
+++ absys/apps/abrechnung/responses.py
@@ -26,10 +26,10 @@ class SaxMBSResponse(HttpResponse):
         with io.StringIO(newline='\r\n') as output:
             counter = 1
             for rechnung_einrichtung in self.rechnung_sozialamt.rechnungen_einrichtungen.all():
-                if rechnung_einrichtung.pers_bkz:
+                if rechnung_einrichtung.einrichtung.pers_bkz:
                     print(self.zeile_rechnung_einrichtung(rechnung_einrichtung, counter), file=output)
                 else:
-                    for schueler in rechnung_einrichtung.schueler.all():
+                    for schueler in rechnung_einrichtung.einrichtung.schueler.all():
                         print(self.zeile_rechnung_schueler(rechnung_einrichtung, schueler, counter), file=output)
                 counter += 1
             print(self.zeile_sicherungsdatensatz(), file=output)
