08513e45041d57f9506193e3e98c79648651b5a8
Eric Goller <eric.goller@ninjaduck.solutions>
2016-12-21 17:09:43 +0100

    Umleitung von dashboard zu 'Anwesenheitsliste'
    Genau dann wenn `anwesenheitsliste.add_anwesenheit`` *und*
    ``anwesenheitsliste.change_anwesenheit`` die einzigen beiden
    Berechtigungen eines Nutzer sind wird er/sie beim versuch auf das
    Dashboard zuzugreifen stattdessen auf die 'Anwesenheitsliste'
    umgeleitet. Dies geschiet insbesondere auch unmittelbar nach dem login
    da das Dashboard als 'Einstiegspunkt' fungiert.
    
    Note:
    - Der Umstand das der redirect nach dem login erfolgt hängt davon ab das
    das dashboard als ``LOGIN_REDIRECT_URL`` gesetzt ist.
    - Dieser Commit macht es unmöglich das user mit nur diesen beiden
    Permissions das Dashboard sehen können. Also nicht nur nicht direkt nach
    dem login.

--- absys/apps/dashboard/views.py
+++ absys/apps/dashboard/views.py
@@ -1,4 +1,5 @@
 from django.http import HttpResponse
+from django.shortcuts import redirect
 
 from django.views.generic import TemplateView
 from braces.views import LoginRequiredMixin
@@ -7,3 +8,16 @@ from braces.views import LoginRequiredMixin
 class DashboardView(LoginRequiredMixin, TemplateView):
 
     template_name = 'dashboard/dashboard.html'
+
+    def dispatch(self, request, *args, **kwargs):
+        """
+        Leite direkt zur Anwesenheitsliste weiter wenn dies die *einzigen* Rechte sind.
+
+        Note:
+            Es geht nicht darum das mindestens ``erwartete_rechte`` vorliegen müssen sondern den
+            speziellen Fall das dies auch die *einzigen* Rechte sind.
+        """
+        erwartete_rechte = ('anwesenheitsliste.add_anwesenheit', 'anwesenheitsliste.change_anwesenheit')
+        if request.user.get_all_permissions() == set(erwartete_rechte):
+            return redirect('anwesenheitsliste_anwesenheit_heute')
+        return super().dispatch(request, *args, **kwargs)
