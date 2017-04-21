00fa6b8bad3cb499df7a398b303a2f2a762df0b1
Eric Goller <eric.goller@ninjaduck.solutions>
2017-04-21 11:13:38 +0200

    abrechnung: Ändere 'zeitraum_anfang_ende' in tag
    Ein custom tag ist der Funktionalität angemessener. Das Template wurde
    entsprechend angepasst.
    
    Ref: ABSYS-2

--- absys/apps/abrechnung/templatetags/abrechnung_tags.py
+++ absys/apps/abrechnung/templatetags/abrechnung_tags.py
@@ -5,3 +5,29 @@ register = template.Library()
 @register.simple_tag
 def get_item(dictionary, key):
     return dictionary.get(key)
+
+
+@register.simple_tag
+def zeitraum_anfang_ende(zeitraum):
+    """
+    Liefert das Anfangs- und Enddatum eines Zeitraums.
+
+    Zu beachten ist hier das evtl. vorhandene "Kontext" Daten *nicht* berücksichtigt werden da
+    sie im Sinne unserer Definition nicht zum 'Darstellungszeitraum' gehören.
+
+    Returns:
+        tuple: (Anfang, Ende)
+    """
+    def get_start(zeitraum):
+        i = 0
+        while zeitraum[i][1] is True:
+            i += 1
+        return zeitraum[i][0]
+
+    def get_ende(zeitraum):
+        i = -1
+        while zeitraum[i][1] is True:
+            i -= 1
+        return zeitraum[i][0]
+
+    return (get_start(zeitraum), get_ende(zeitraum))
